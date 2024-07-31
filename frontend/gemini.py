99a94c0f187147edbb4355ff286bb026539f7ec3
Nav235 <c0895180@mylambton.ca>
2024-07-31 23:59:49 +0000

    1

--- frontend/gemini.py
+++ frontend/gemini.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 
 app = Flask(__name__)
 
-API_KEY = "AIzaSyC9uXy92lQX2knCanapxwVfEHRixkpG8rM"
+API_KEY = "AIzaSyCaRZ1VYHRrvfIsxG4nC9-w-6kLR7cTD1E"
 genai.configure(api_key=API_KEY)
 
 HTML_CONTENT = '''
